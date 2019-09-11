c67893c953746c2c3377f3b5fbf746d9574450a4
Vedran <vedranknezevic1@yahoo.com>
2019-09-11 22:45:22 +0200

    working on forminput and custom-button component, and replacing old elements in signinpage with it

---
+++ src/components/signin/signin.component.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+
+import "./signin.styles.scss";
+
+import FormInput from "../../components/form-input/form-input.component";
+import CustomButton from "../../components/custom-button/custom-button.component";
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = e => {
+    const { value, name } = e.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+  render() {
+    return (
+      <div className="sign-in">
+        <h2 className="title">I already have an account</h2>
+        <span>Sign in with your email</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="email"
+            required
+          />
+
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="password"
+            required
+          />
+
+          <CustomButton type="submit">Sign In</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
